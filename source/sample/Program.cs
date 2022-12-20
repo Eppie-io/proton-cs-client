db3ccfe9f1ad349364124b0a8592c50bd12c39f0
alexander <116556921+al-kau@users.noreply.github.com>
2022-12-20 16:14:06 +0200

    init solution (#1)
    * add submodules
    
    * init solution

---
+++ source/sample/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tuvi.Toolkit.Cli;
+
+namespace Tuvi.Proton.Client.Sample
+{
+    internal static class Program
+    {
+        private static string Hello { get; } = "Hello, Proton!";
+
+        private static void Main()
+        {
+            ConsoleExtension.WriteLine(Hello, ConsoleColor.Green);
+        }
+    }
+}
